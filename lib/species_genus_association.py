287ee3e7130b468ce930298ef08618b95ffe191c
timhunuig <Timothy.Hulshof@nuigalway.ie>
2022-04-07 16:52:46 +0100

    minor

--- lib/species_genus_association.py
+++ lib/species_genus_association.py
@@ -10,5 +10,5 @@ def association(df, levels, level):
     df_associated_species = df_associated_species.drop(columns=levels_copy).groupby(level).sum()
     df_associated_genus = df_associated_genus.drop(columns=levels_copy).groupby(level).sum()
 
-    return  df_associated_species, df_associated_genus
+    return df_associated_species, df_associated_genus
 
