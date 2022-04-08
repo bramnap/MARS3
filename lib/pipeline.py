e51d1d5e4746c72d37a58900c0c2f3c5e1a4ed68
timhunuig <Timothy.Hulshof@nuigalway.ie>
2022-04-08 10:47:41 +0100

    association

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -9,8 +9,8 @@ def pipeline(df, total_df, levels, level, agora2_level_set, agora2_species, agor
     total_df_species_in_agora = df[df["Species"].isin(agora2_species)]
     total_df_genera_in_agora = df[df["Genus"].isin(agora2_genera)]
 
-    associated_species_agora2 = species_genus_association.association(total_df_species_in_agora, levels, level)
-    associated_genus_agora2 = species_genus_association.association(total_df_genera_in_agora, levels, level)
+    associated_species_agora2, _ = species_genus_association.association(total_df_species_in_agora, levels, level)
+    _, associated_genus_agora2 = species_genus_association.association(total_df_genera_in_agora, levels, level)
 
     #save these files
     total_df.to_csv(f'MARS_output/total_{level}.csv')
