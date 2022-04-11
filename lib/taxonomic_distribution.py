fff217a934c56f864f8675c7fa1adcccd75a58ea
Bram Nap <70942965+bramnap@users.noreply.github.com>
2022-04-11 10:56:30 +0100

    xx

--- lib/taxonomic_distribution.py
+++ lib/taxonomic_distribution.py
@@ -9,5 +9,5 @@ def taxonomic_distribution(total_reads, absent, present, agora2_level_set, df, l
     levels_copy.append("Kingdom") #for dropping
     df_present_species = df_present_species.drop(columns=levels_copy).groupby(level).sum()
     df_absent_agora_species, df_present_agora_species = agora_checking.agora_checking(df_present_species, agora2_level_set)
-    
+
     return df_present_species, df_present_agora_species, df_absent_agora_species, absent/total_reads
